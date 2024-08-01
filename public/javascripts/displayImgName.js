f862e71604ce594b0278a07011ada2f3b23fd556
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2024-08-01 21:47:17 +0400

    auto reinitialize preview select.file

--- public/javascripts/displayImgName.js
+++ public/javascripts/displayImgName.js
@@ -1,6 +1,7 @@
 function previewMultiple(event) {
     const images = document.getElementById("image");
     const number = images.files.length;
+    document.getElementById("formFile").innerHTML = '';
     for (i = 0; i < number; i++) {
         const urls = URL.createObjectURL(event.target.files[i]);
         document.getElementById("formFile").innerHTML += `<img src="${urls}">`;
