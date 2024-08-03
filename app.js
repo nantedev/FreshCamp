d1844a09ff34faf4a22a0c955af2026510a647c6
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2024-08-03 18:57:36 +0400

    Fix security issues

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const helmet = require('helmet');
+
+const mongoSanitize = require('express-mongo-sanitize');
 
 const userRoutes = require('./routes/users')
 const campgroundRoutes = require('./routes/campgrounds');
@@ -36,20 +39,27 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true}));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(
+    mongoSanitize({
+      replaceWith: '_',
+    }),
+  );
 
 const sessionConfig = {
+    name: 'session',
     secret: 'thisisbettersecret',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        // secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
 };
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet());
 
 app.use(passport.initialize());
 app.use(passport.session());
