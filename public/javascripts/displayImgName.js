a212b04748344d0d638e0844ca11ee252a9b8910
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2024-08-05 08:43:19 +0400

    crossorigin..

--- public/javascripts/displayImgName.js
+++ public/javascripts/displayImgName.js
@@ -4,6 +4,6 @@ function previewMultiple(event) {
     document.getElementById("formFile").innerHTML = '';
     for (i = 0; i < number; i++) {
         const urls = URL.createObjectURL(event.target.files[i]);
-        document.getElementById("formFile").innerHTML += `<img src="${urls}">`;
+        document.getElementById("formFile").innerHTML += `<img crossorigin="anonymous" src="${urls}">`;
     }
 }
